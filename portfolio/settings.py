af7a893b8e530b2f5b8b7363b28712182cdb03ea
musmana <shinymusmana@gmail.com>
2025-11-05 23:02:07 +0530

    css correction

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -40,8 +40,6 @@ INSTALLED_APPS = [
     'main',
 ]
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "main" / "static"]
 
 
 MIDDLEWARE = [
@@ -116,16 +114,20 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# ✅ Add these if not already present
+STATIC_URL = '/static/'
+
+# Location for your collected static files (used by collectstatic)
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Additional locations for static files
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
